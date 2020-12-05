eecb5e31f550b06bbbc4d818ea992af25bee72d9
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-05 22:28:09 +0900

    正解or不正解を表示

--- typing.go
+++ typing.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
 	ask("dog")
 	ask("cat")
 	ask("fish")
@@ -13,4 +12,10 @@ func ask(question string) {
 	var input string
 	fmt.Printf("次の単語を入力してください: %s\n", question)
 	fmt.Scan(&input)
+
+	if question == input {
+		fmt.Println("正解!")
+	} else {
+		fmt.Println("不正解!")
+	}
 }
