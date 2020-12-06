8f5c4ae990759783c2afb8bc634d1a9bf70664ca
matsufuji06 <n.matsufuji06@gmail.com>
2020-12-06 12:13:04 +0900

    所用時間を表示できるようにした

--- typing.go
+++ typing.go
@@ -1,13 +1,20 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
+	start := time.Now();
+
 	totalScore := ask(1, "dog")
 	totalScore += ask(2, "cat")
 	totalScore += ask(3, "fish")
 
+	end := time.Now()
+
+	fmt.Printf("%f秒かかりました\n", (end.Sub(start)).Seconds())
 	fmt.Println("スコア", totalScore)
+	
 }
 
 func ask(number int, question string) int {
